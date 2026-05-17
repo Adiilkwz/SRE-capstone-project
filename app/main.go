7aca30b21a3b431fa403326cd9e9a7cfd1bb27a6
cureeeeee <kilowcool@gmail.com>
2026-05-17 21:09:30 +0500

    feat: deploying grafana and prometheus, alertmanager with alert_rules

--- app/main.go
+++ app/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+  "fmt"
+  "log"
+  "net/http"
+  "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Service is healthy and ready for traffic!")
+  w.WriteHeader(http.StatusOK)
+  fmt.Fprintf(w, "Service is healthy and ready for traffic!")
 }
 
 func main() {
-	http.HandleFunc("/health", healthHandler)
-	
-	fmt.Println("Server starting on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+  http.HandleFunc("/health", healthHandler)
+  
+  http.Handle("/metrics", promhttp.Handler()) 
+  
+  fmt.Println("Server starting on port 8080...")
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Fatalf("Server failed to start: %v", err)
+  }
 }
